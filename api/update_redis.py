1b68dcd409fe050444da9466dbdd5f0b76657089
golubevbitrix <golubevbitrix@gmail.com>
2025-06-18 14:26:59 +0500

    Update update_redis.py

--- api/update_redis.py
+++ api/update_redis.py
@@ -48,7 +48,7 @@ async def redis_update_handler():
             if timestamp - int(row["time"]) > 240 and timestamp - int(row["time"]) < 1000:
                 print('key: ', type(key))
                 print('secs: ', int(row["time"]) - timestamp)
-                print('queue: ', lines[row["line"]], len()
+                print('queue: ', lines[row["line"]])
                 user = row["user"]
                 queue = lines[row["line"]]
                 print('queue: ', lines[row["line"]], len(queue))
