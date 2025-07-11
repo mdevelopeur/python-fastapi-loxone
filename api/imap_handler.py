9aa1ca882e32f7f63d037e9a6ae8344e2fd2ae55
boouee <104202645+boouee@users.noreply.github.com>
2025-07-11 16:29:40 +0500

    Update imap_handler.py

--- api/imap_handler.py
+++ api/imap_handler.py
@@ -1,1 +1,9 @@
+from imap_tools import MailBox, AND
 
+address = "crm@apexdiabetes.ru"
+password = "1Mcl552smPjUsPXu"
+server = "mail.netangels.ru"
+# Get date, subject and body len of all emails from INBOX folder
+with MailBox(server).login(address, password) as mailbox:
+    for msg in mailbox.fetch():
+        print(msg.date, msg.subject, len(msg.text or msg.html))
