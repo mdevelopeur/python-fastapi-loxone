49aa65c349d4dae79084bc7f1ac4c91aeec09f57
boouee <104202645+boouee@users.noreply.github.com>
2025-07-15 12:39:01 +0500

    Update imap_handler.py

--- api/imap_handler.py
+++ api/imap_handler.py
@@ -21,17 +21,22 @@ async def imap_handler():
             html = str(msg.html)
             print(len(html))
             print("Name" in html)
-            name = re.findall("Name:(.*)<br>", html)    
-            phone = re.findall("Phone:(.*)<br>", html)
-            email = re.findall("Email:(.*)<br>", html)
-            comments = re.findall("Input:(.*)<br>", html)
+            name = re.findall("Name:(.*)<br>", html)[0]    
+            phone = re.findall("Phone:(.*)<br>", html)[0]
+            email = re.findall("Email:(.*)<br>", html)[0]
+            comments = re.findall("Input:(.*)<br>", html)[0]
+            
+            input_2 = re.findall("Input_2:(.*)<br>", html)
+            if len(input_2) > 0:
+                comments += ("\n" + input_2[0]
+            #+ "\n" + re.findall("Input_2:(.*)<br>", html)
             print(name, phone, email, comment)
             mailbox.move(msg.uid, "INBOX.Trash")
             await create_deal(name)
 
 async def create_deal(name, phone, email, comments):
     async with httpx.AsyncClient() as client:
-        data = {"fields": {"TITLE": name, "UF_CRM_1723114789182": name, "UF_CRM_1723114805999": email, "UF_CRM_1723114796732": phone, "COMMENTS": comments   }}
+        data = {"fields": {"TITLE": name, "CATEGORY_ID": 12, "UF_CRM_1723114789182": name, "UF_CRM_1723114805999": email, "UF_CRM_1723114796732": phone, "COMMENTS": comments   }}
         response = await client.post(f"{api}crm.deal.add", json=data)
 
 async def get_data(text):
