53b927b57928bef91466a3d195b8c6963dc646c4
boouee <104202645+boouee@users.noreply.github.com>
2025-08-04 15:09:17 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.imap_handler import imap_handler
+from api.functions import hook_handler
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
