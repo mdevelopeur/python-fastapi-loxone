41fb685c2c9bbd6e282b14477dcec4a1d160143b
boouee <104202645+boouee@users.noreply.github.com>
2025-08-04 15:29:42 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -23,7 +23,7 @@ redis_url = os.getenv("REDIS_URL")
 async def hook_handler(request):
   async with httpx.AsyncClient() as client:
     url = "https://edef5.pcloud.com/DLZFWa8d6Z1pCUbu7ZAT2fZZP6vlVkZf7ZZlK4ZZFsFZjpZBJZhRZcDnYEyFWkG7qo6mR3Bf3UpWzrtRy/%D0%9E%D1%82%D1%87%D0%B5%D1%82%20-%20%D0%9A%D0%90%D0%9C%20%D0%9A%D0%B0%D0%BB%D0%B8%D1%87%D0%BA%D0%B8%D0%BD%20%D0%94.%D0%A1.xlsx"
-    response = client.get(url)
+    response = await client.get(url)
     file = io.BytesIO(response.content)
     try:
       df = pd.read_excel(file, engine='openpyxl')
