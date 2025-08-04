bb38e5148a33c586979eec146b09cfb7e3b061e2
boouee <104202645+boouee@users.noreply.github.com>
2025-08-04 15:53:15 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -22,7 +22,7 @@ redis_url = os.getenv("REDIS_URL")
 
 async def hook_handler(request):
   async with httpx.AsyncClient() as client:
-    url = "https://edef5.pcloud.com/DLZFWa8d6Z1pCUbu7ZAT2fZZP6vlVkZf7ZZlK4ZZFsFZjpZBJZhRZcDnYEyFWkG7qo6mR3Bf3UpWzrtRy/%D0%9E%D1%82%D1%87%D0%B5%D1%82%20-%20%D0%9A%D0%90%D0%9C%20%D0%9A%D0%B0%D0%BB%D0%B8%D1%87%D0%BA%D0%B8%D0%BD%20%D0%94.%D0%A1.xlsx"
+    url = await get_link(client, "71220424309")
     response = await client.get(url)
     file = io.BytesIO(response.content)
     try:
@@ -32,12 +32,8 @@ async def hook_handler(request):
       print(f"Error reading Excel file: {e}")
 
 
-async def chat_id(code):
-  async with httpx.AsyncClient() as client:
-    data = {"USER_CODE": code}
-    response = await client.post(api +'imopenlines.dialog.get', data=data)
-    response = response.json()
-    print('chatId: ', response)
-    connector = response["result"]["entity_id"].split("|")[0]
-    return {"chat": str(response["result"]["id"]), "user": str(response["result"]["owner"]), "connector": connector}
-    
+async def get_link(client, fileid):
+  url = "https://eapi.pcloud.com/getfilelink?fileid=" + fileid
+  response = await client.get(url, headers=headers)
+  response = response.json()
+  return response["hosts"][0] + response["path"]
