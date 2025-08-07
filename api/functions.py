86bb9904c77abda3355fbe4c1e30149018c3b187
boouee <104202645+boouee@users.noreply.github.com>
2025-08-07 20:32:22 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -12,13 +12,13 @@ import pandas as pd
 import openpyxl
 import io
 
-
 load_dotenv(dotenv_path=".env")
 api = os.getenv("api")
 connection_string = os.getenv("postgresql")
 slist = os.getenv("list").split(',')
 load_dotenv(dotenv_path=".env.local")
 redis_url = os.getenv("REDIS_URL")
+eapi = "https://eapi.pcloud.com/"
 token = "AT2fZ89VHkDT7OaQZMlMlVkZdslpGwQPJNbTKpnbvQtbO8yBYcny"
 headers = {"Authorization": f"Bearer {token}"}
 
@@ -33,9 +33,24 @@ async def hook_handler(request):
     except Exception as e:
       print(f"Error reading Excel file: {e}")
 
-
 async def get_link(client, fileid):
-  url = "https://eapi.pcloud.com/getfilelink?fileid=" + fileid
+  url = eapi + "getfilelink?fileid=" + fileid
   response = await client.get(url, headers=headers)
   response = response.json()
   return "https://" + response["hosts"][0] + response["path"]
+
+async def list_folders(client):
+  url = eapi + "listfolder?path=/Отчеты КАМ"
+  response = await client.get(url, headers=headers)
+  response = response.json()
+  folders = list(filter(lambda folder: folder["isfolder"], response["metadata"]["contents"]))
+  folders = list(map(get_folder_data, folders))
+  return folders 
+  
+def get_folder_data(folder):
+  updated = folder["modified"].split(' ')
+  updated = ' '.join(updated[1:5])
+  updated = datetime.strptime(updated, "%d %B %Y %H %M %S")
+  print(updated)
+  return {"id": folder["id"], "name": folder["name"], "updated": updated}
+  
