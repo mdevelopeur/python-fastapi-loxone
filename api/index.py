3615adeef98bccc86ef983c3350242d5e0f9bc5e
boouee <104202645+boouee@users.noreply.github.com>
2025-08-11 15:35:03 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import hook_handler
+from api.functions import main
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
@@ -9,7 +9,7 @@ app = FastAPI()
 @app.get('/api/update')
 async def update(request: Request):
     try:
-        await hook_handler(request)
+        await main()
     except Exception as e:
         print(e)
 
