0a440ece26882b64a204187fff0d6481c75bf812
boouee <104202645+boouee@users.noreply.github.com>
2025-08-12 17:04:15 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -185,5 +185,7 @@ def format_headers(df):
   print(headers)
   headers = df.columns.tolist()
   print(headers)
-  headers = list(map(lambda header: header.strip().upper(), headers))
+  formatted_headers = list(map(lambda header: header.strip().upper(), headers))
   print(headers)
+  df.rename(columns=dict(zip(headers, formatted_headers)))
+  print(list(df.keys()))
