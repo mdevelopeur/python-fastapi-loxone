e50f1888df43fbcf4413ea68f58062a7ff4f817f
boouee <104202645+boouee@users.noreply.github.com>
2025-08-20 12:04:10 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import main
+from api.functions import main, clear_redis, deduplicate
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
 
-@app.get('/api/update')
+@app.get('/api/update_')
 async def update(request: Request):
     try:
         await main()
     except Exception as e:
         print(e)
 
+@app.get('/api/clear_redis')
+async def update(request: Request):
+    try:
+        await clear_redis()
+    except Exception as e:
+        print(e)
+
+@app.get('/api/deduplicate')
+async def update(request: Request):
+    try:
+        await deduplicate()
+    except Exception as e:
+        print(e)
