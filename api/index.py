ba8eed22dbd9cd7a7f331efa6d30e8009b6d6e89
boouee <104202645+boouee@users.noreply.github.com>
2025-09-12 14:10:36 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import main, clear_redis, deduplicate
+from api.functions import main
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
