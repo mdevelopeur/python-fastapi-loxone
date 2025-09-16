f5178ea62d8b4d499c4437f5a2bb19c6902aabbb
boouee <104202645+boouee@users.noreply.github.com>
2025-09-17 01:06:06 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -128,12 +128,12 @@ async def get_documents(client, products):
     if "S" not in documents:
       if list(filter(lambda store: store["store"] == -1, product["storeAmounts"])):     
         document = await create_document(client, "S")
-        documents["S"] == document
+        documents["S"] = document
         print(documents)
     if "M" not in documents:
       if list(filter(lambda store: store["store"] != -1, product["storeAmounts"])):
         document = await create_document(client, "M")
-        documents["M"] == document
+        documents["M"] = document
         print(documents)
     if len(documents.keys()) > 1:
       break
