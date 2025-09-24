6026f0fcb19fc4e8e78b8af8e27b6261a6ef5c3d
mdevelopeur <mdevelopeur@gmail.com>
2025-09-24 18:53:44 +0500

    Update handlers.py

--- api/handlers.py
+++ api/handlers.py
@@ -12,13 +12,15 @@ import string
 import random 
 from dotenv import load_dotenv
 
+password = os.getenv("password")
+default_password = os.getenv("default_password")
 load_dotenv(dotenv_path=".env.local")
 redis_url = os.getenv("REDIS_URL")
 api = os.getenv("api")
 target_store = 59
 eapi = "https://eapi.pcloud.com/"
 token = "AT2fZ89VHkDT7OaQZMlMlVkZdslpGwQPJNbTKpnbvQtbO8yBYcny"
-headers = {"Authorization": f"Bearer {token}"}
+headers = {"Authorization": f"Bearer admin:{password}"}
 
 async def set_time(time):  
   r = redis.Redis.from_url(redis_url, decode_responses=True)
@@ -45,3 +47,7 @@ async def set_password(password):
   async with httpx.AsyncClient() as client:
     output = await client.get(url, headers=headers)
     return output
+
+def generate_password():
+  password = ''.join(secrets.choice(string.digits) for i in range(10))
+  return password
