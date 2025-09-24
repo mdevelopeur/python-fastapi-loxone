cf0f55ca4d035d581699fa4b16d5d52a7dc4014c
mdevelopeur <mdevelopeur@gmail.com>
2025-09-24 21:00:40 +0500

    Update handlers.py

--- api/handlers.py
+++ api/handlers.py
@@ -52,7 +52,7 @@ async def update():
   seconds = time.second
   time = time - timedelta(seconds=seconds) + timedelta(hours=3)
   print(time)
-  timestamp = "loxone:" + str(int(datetime.now().timestamp()))
+  timestamp = "loxone:" + str(int(time.timestamp()))
   print(timestamp)
   data = r.hgetall(timestamp)
   print(data)
