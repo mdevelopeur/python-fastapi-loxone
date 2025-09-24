191a10e50d3887ca97c74600d5177be9f42e6e99
mdevelopeur <mdevelopeur@gmail.com>
2025-09-24 18:24:40 +0500

    Update and rename handler.py to handlers.py

---
+++ api/handlers.py
@@ -0,0 +1,28 @@
+from urllib.parse import unquote
+from datetime import datetime
+import httpx
+import re
+import math
+import numbers
+import time
+import os
+import redis
+import secrets
+import string
+import random 
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.local")
+redis_url = os.getenv("REDIS_URL")
+api = os.getenv("api")
+target_store = 59
+eapi = "https://eapi.pcloud.com/"
+token = "AT2fZ89VHkDT7OaQZMlMlVkZdslpGwQPJNbTKpnbvQtbO8yBYcny"
+headers = {"Authorization": f"Bearer {token}"}
+
+async def set(time):  
+  r = redis.Redis.from_url(redis_url, decode_responses=True)
+  timestamp = datetime.strptime(time, "").timestamp()
+  return timestamp
+  
+  async with httpx.AsyncClient() as client:
