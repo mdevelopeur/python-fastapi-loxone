05a7fb16b5b93771e1977935664f487ddc738294
mdevelopeur <mdevelopeur@gmail.com>
2025-09-24 18:27:39 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, Request
 import multipart
 import re
-from api.handlers import set, update
+from api.handlers import set_time, update
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
 
-@app.get('/api/set')
-async def set(date: str, time: str):
+@app.get('/api/set_time')
+async def set_time(date: str, time: str):
     try:
         output = await set(f"{date} {time}")
         print(output)
